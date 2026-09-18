c11ade16c354a0a5a9be8578ab01be8c94a1ec7d
Jakob von Gehlen <220725756+jakobvongehlen@users.noreply.github.com>
2026-09-18 10:20:22 +0000

    Add notifications service test container with entrypoint script and README

---
+++ utils/notifications-service-test-container/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+curl -X 'POST' \
+  'http://localhost:3000/api/notify' \
+  -H 'accept: text/plain; charset=utf-8' \
+  -H 'Content-Type: application/json; charset=utf-8' \
+  -d '{
+      "notification_type": "OutOfRange",
+      "researcher": "d.landau@uu.nl",
+      "measurement_id": "1234",
+      "experiment_id": "5678",
+      "cipher_data": "D5qnEHeIrTYmLwYX.hSZNb3xxQ9MtGhRP7E52yv2seWo4tUxYe28ATJVHUi0J++SFyfq5LQc0sTmiS4ILiM0/YsPHgp5fQKuRuuHLSyLA1WR9YIRS6nYrokZ68u4OLC4j26JW/QpiGmAydGKPIvV2ImD8t1NOUrejbnp/cmbMDUKO1hbXGPfD7oTvvk6JQVBAxSPVB96jDv7C4sGTmuEDZPoIpojcTBFP2xA"
+  }' > /output/response.txt
---
+++ utils/notifications-service-test-container/readme.md
@@ -0,0 +1,3 @@
+According to the cec-tutorial "Create a shortlived container that sends a single request to the notifications-service..." we need to have this test container ready for the evaulaiton of the lab assignment.
+
+"The lab assignments will be assessed during the tutorials on the 24th of September."
